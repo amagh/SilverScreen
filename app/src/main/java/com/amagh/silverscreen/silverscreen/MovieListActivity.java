1516d51553af349ad540e90b5b66d79afcd383c6
A.Magh <amagh1990@gmail.com>
2017-06-01 14:07:52 -0700

    Add FetchMoviesTask for fetching Movie information
    Inner class of MoviesListActivity that fetches Movie data and stores it
    as a List<Movies>.

--- app/src/main/java/com/amagh/silverscreen/silverscreen/MovieListActivity.java
+++ app/src/main/java/com/amagh/silverscreen/silverscreen/MovieListActivity.java
@@ -2,13 +2,36 @@ package com.amagh.silverscreen.silverscreen;
 
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.IntDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class MovieListActivity extends AppCompatActivity {
     // Constants
     private final String TAG = MovieListActivity.class.getSimpleName();
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef ({POPULAR, RATING})
+    @interface SortMethod {}
+    public static final int POPULAR = 0;
+    public static final int RATING = 1;
 
     // Mem Vars
     private RecyclerView mRecyclerView;
@@ -19,7 +42,132 @@ public class MovieListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_list);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.movie_list_rv);
+
+        FetchMoviesTask moviesTask = new FetchMoviesTask(POPULAR);
+        moviesTask.execute();
     }
 
+    private class FetchMoviesTask extends AsyncTask<Void, Void, List<Movie>> {
+        // Constants
+        private final String TMDB_BASE_URL = "https://api.themoviedb.org/3/";
+        private final String TMBD_MOVIE_PATH = "movie";
+        private final String TMDB_POPULAR_PATH = "popular";
+        private final String TMDB_TOP_RATED_PATH = "top_rated";
+        private final String TMDB_API_QUERY = "api_key";
+        private final String TMDB_API_KEY = BuildConfig.API_KEY;
 
+        // Mem Vars
+        private URL mBuiltUrl;
+
+        public FetchMoviesTask(@SortMethod int sortMethod) {
+            // Build the URL for accessing TMDB API depending on what sort method the user has
+            // selected
+
+            String sortPath;
+            if (sortMethod == POPULAR) {
+                sortPath = TMDB_POPULAR_PATH;
+            } else {
+                sortPath = TMDB_TOP_RATED_PATH;
+            }
+
+            Uri uri = Uri.parse(TMDB_BASE_URL).buildUpon()
+                    .appendPath(TMBD_MOVIE_PATH)
+                    .appendPath(sortPath)
+                    .appendQueryParameter(TMDB_API_QUERY, TMDB_API_KEY)
+                    .build();
+
+            try {
+                mBuiltUrl = new URL(uri.toString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected List<Movie> doInBackground(Void... voids) {
+            if (mBuiltUrl == null) {
+                // If URL was not built, then nothing to do
+                return null;
+            }
+
+            // Init outside of try-catch block to be closed in finally block
+            HttpURLConnection urlConnection = null;
+            Scanner scanner = null;
+
+            try {
+                // Open connection and retrieve the HTML document
+                urlConnection = (HttpURLConnection) mBuiltUrl.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+
+                // Parse the document for the JSONObject
+                scanner = new Scanner(inputStream);
+                scanner.useDelimiter("\\A");
+
+                return getMoviesFromJson(scanner.next());
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            } finally {
+                // Close open connections
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (scanner != null) {
+                    scanner.close();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> movies) {
+            if (movies != null && movies.size() > 0) {
+                // Add movies to Adapter
+                for (Movie movie : movies) {
+                    Log.d(TAG, "Movie title; " + movie.getTitle());
+                }
+            }
+        }
+
+        List<Movie> getMoviesFromJson(String jsonString) throws JSONException {
+            // JSON parsing Strings
+            String jsonResultsArray = "results";
+            String jsonPoster = "poster_path";
+            String jsonOverview = "overview";
+            String jsonReleaseDate = "release_date";
+            String jsonTitle = "title";
+            String jsonBackdrop = "backdrop_path";
+
+            String jsonId = "id";
+            String jsonVoteCount = "vote_count";
+            String jsonVoteAverage = "vote_average";
+
+            // Convert String to JSONObject
+            JSONObject movieJson = new JSONObject(jsonString);
+
+            JSONArray resultsArray = movieJson.getJSONArray(jsonResultsArray);
+
+            // Init List to hold Movie Objects
+            List<Movie> movieList = new ArrayList<>();
+
+            // Iterate, create Movie Objects, and add them to the List
+            for (int i = 0; i < resultsArray.length(); i++) {
+                JSONObject movieObject = resultsArray.getJSONObject(i);
+
+                Movie movie = new Movie();
+                movie.setPosterPath(movieObject.getString(jsonPoster));
+                movie.setOverview(movieObject.getString(jsonOverview));
+                movie.setReleaseDate(movieObject.getString(jsonReleaseDate));
+                movie.setTitle(movieObject.getString(jsonTitle));
+                movie.setBackdropPath(movieObject.getString(jsonBackdrop));
+                movie.setId(movieObject.getInt(jsonId));
+                movie.setVoteCount(movieObject.getInt(jsonVoteCount));
+                movie.setVoteAverage(movieObject.getDouble(jsonVoteAverage));
+
+                movieList.add(movie);
+            }
+
+
+            return movieList;
+        }
+    }
 }
