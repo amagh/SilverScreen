0dc0df884209d2b875186a8f3e336be6943062fd
A.Magh <amagh1990@gmail.com>
2017-06-01 14:06:45 -0700

    Add Class Movie
    Movie will be used to create Movie Objects to store Movie information
    to pass along to the Recycler.Adapter and the ActivityDetails when they
    are ready.

---
+++ app/src/main/java/com/amagh/silverscreen/silverscreen/Movie.java
@@ -0,0 +1,100 @@
+package com.amagh.silverscreen.silverscreen;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by hnoct on 6/1/2017.
+ */
+
+public class Movie implements Parcelable{
+    private String posterPath;
+    private String overview;
+    private String releaseDate;
+    private String title;
+    private String backdropPath;
+
+    private int id;
+    private int voteCount;
+
+    private double voteAverage;
+
+    public Movie() {
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+    }
+
+    // Getters and Setters
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
