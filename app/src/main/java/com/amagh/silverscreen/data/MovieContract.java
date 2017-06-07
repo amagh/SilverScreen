b3d0a2c4a8f3b1fe3d8cbd947329b059eed34f15
A.Magh <amagh1990@gmail.com>
2017-06-07 16:45:40 -0700

    Modify database schema
    Added a column for favorites under the 'movies' table and a new table
    for trailers.

--- app/src/main/java/com/amagh/silverscreen/data/MovieContract.java
+++ app/src/main/java/com/amagh/silverscreen/data/MovieContract.java
@@ -8,49 +8,70 @@ import android.provider.BaseColumns;
  */
 
 public class MovieContract {
-    // Constants
+    // **Constants** //
     public static final String AUTHORITY = "com.amagh.silverscreen";
+
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
     public static final String PATH_MOVIES = "movies";
     public static final String PATH_GENRES = "genres";
+    public static final String PATH_TRAILERS = "trailer";
+
     public static final String PATH_LINK_GENRES_MOVIES = "link_genres_movies";
+    public static final String PATH_LINK_TRAILERS_MOVIES = "link_trailers_movies";
 
     public static class MovieEntry implements BaseColumns {
         // Content URI for accessing table information
-        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
-                .appendPath(PATH_MOVIES).build();
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
 
         // Table information
         public static final String TABLE_NAME = "movies";
 
         // Columns
-        public static final String COLUMN_MOVIE_ID = "movie_id";
-        public static final String COLUMN_TITLE = "title";
-        public static final String COLUMN_RELEASE_DATE = "release_date";
-        public static final String COLUMN_POSTER_PATH = "poster_path";
-        public static final String COLUMN_BACKDROP_PATH = "backdrop_path";
-        public static final String COLUMN_VOTE_COUNT = "vote_count";
-        public static final String COLUMN_VOTE_AVG = "vote_average";
-        public static final String COLUMN_SYNOPSIS = "synposis";
+        public static final String COLUMN_MOVIE_ID          = "movie_id";
+        public static final String COLUMN_TITLE             = "title";
+        public static final String COLUMN_RELEASE_DATE      = "release_date";
+        public static final String COLUMN_POSTER_PATH       = "poster_path";
+        public static final String COLUMN_BACKDROP_PATH     = "backdrop_path";
+        public static final String COLUMN_VOTE_COUNT        = "vote_count";
+        public static final String COLUMN_VOTE_AVG          = "vote_average";
+        public static final String COLUMN_SYNOPSIS          = "synposis";
+        public static final String COLUMN_FAVORITE          = "favorite";
     }
 
     public static class GenreEntry implements BaseColumns {
         // Content URI for accessing table information
-        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
-                .appendPath(PATH_GENRES).build();
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_GENRES).build();
 
         // Table information
         public static final String TABLE_NAME = "genres";
 
         // Columns
-        public static final String COLUMN_GENRE_ID = "genre_id";
-        public static final String COLUMN_GENRE = "genre";
+        public static final String COLUMN_GENRE_ID          = "genre_id";
+        public static final String COLUMN_GENRE             = "genre";
+    }
+
+    public static class TrailerEntry implements BaseColumns {
+        // Content URI for accessing table information
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TRAILERS).build();
+
+        // Table information
+        public static final String TABLE_NAME = "trailers";
+
+        // Columns
+        public static final String COLUMN_TRAILER_ID        = "trailer_id";
+        public static final String COLUMN_KEY               = "key";
+        public static final String COLUMN_NAME              = "name";
+        public static final String COLUMN_TYPE              = "type";
     }
 
     public static class LinkGenresMovies implements BaseColumns {
         // Content URI for accessing table information
-        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
-                .appendPath(PATH_LINK_GENRES_MOVIES).build();
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LINK_GENRES_MOVIES).build();
 
         // Table information
         public static final String TABLE_NAME = "link_genres_movies";
--- app/src/main/java/com/amagh/silverscreen/data/MovieDbHelper.java
+++ app/src/main/java/com/amagh/silverscreen/data/MovieDbHelper.java
@@ -12,34 +12,48 @@ import static com.amagh.silverscreen.data.MovieContract.*;
  */
 
 public class MovieDbHelper extends SQLiteOpenHelper {
-    // Constants
-    public static final String DATABASE_NAME = "movies.db";
+    // **Constants** //
+    private static final String DATABASE_NAME = "movies.db";
     private static final int DATABASE_VERSION = 1;
 
     // SQL Statements
-    private final String CREATE_MOVIES_TABLE =
-            "CREATE TABLE " + MovieEntry.TABLE_NAME           + " (" +
+    private static final String CREATE_MOVIES_TABLE =
+            "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
 
             MovieEntry._ID                  + " INTEGER PRIMARY KEY AUTOINCREMENT, "    +
             MovieEntry.COLUMN_MOVIE_ID      + " INTEGER NOT NULL, "                     +
             MovieEntry.COLUMN_TITLE         + " TEXT NOT NULL, "                        +
-            MovieEntry.COLUMN_RELEASE_DATE  + " REAL NOT NULL, "                        +
+            MovieEntry.COLUMN_RELEASE_DATE  + " TEXT NOT NULL, "                        +
             MovieEntry.COLUMN_POSTER_PATH   + " TEXT NOT NULL, "                        +
             MovieEntry.COLUMN_BACKDROP_PATH + " TEXT NOT NULL, "                        +
             MovieEntry.COLUMN_VOTE_COUNT    + " INTEGER, "                              +
             MovieEntry.COLUMN_VOTE_AVG      + " REAL, "                                 +
             MovieEntry.COLUMN_SYNOPSIS      + " TEXT NOT NULL, "                        +
+            MovieEntry.COLUMN_FAVORITE      + " INTEGER, "                              +
 
             "UNIQUE (" + MovieEntry.COLUMN_MOVIE_ID + ") ON CONFLICT REPLACE);";
 
-    private final String CREATE_GENRES_TABLE =
+    private static final String CREATE_GENRES_TABLE =
             "CREATE TABLE " + GenreEntry.TABLE_NAME + " (" +
 
             GenreEntry._ID              + " INTEGER PRIMARY KEY AUTOINCREMENT, "        +
             GenreEntry.COLUMN_GENRE_ID  + " INTEGER NOT NULL, "                         +
             GenreEntry.COLUMN_GENRE     + " TEXT NOT NULL);";
 
-    private final String CREATE_LINK_GENRES_MOVIES_TABLE =
+    private static final String CREATE_TRAILERS_TABLE =
+            "CREATE TABLE " + TrailerEntry.TABLE_NAME + "{ "                            +
+            TrailerEntry._ID                + " INTEGER PRIMARY KEY AUTOINCREMENT, "    +
+            TrailerEntry.COLUMN_TRAILER_ID  + " INTEGER NOT NULL, "                     +
+            TrailerEntry.COLUMN_KEY         + " TEXT NOT NULL "                         +
+            TrailerEntry.COLUMN_NAME        + " TEXT, "                                 +
+            TrailerEntry.COLUMN_TYPE        + " TEXT, "                                 +
+            MovieEntry.COLUMN_MOVIE_ID      + " INTEGER NOT NULL, "                     +
+
+            "FOREIGN KEY (" + MovieEntry.COLUMN_MOVIE_ID + ") " +
+            "REFERENCES " + MovieEntry.TABLE_NAME + "(" + MovieEntry.COLUMN_MOVIE_ID + "));";
+
+
+    private static final String CREATE_LINK_GENRES_MOVIES_TABLE =
             "CREATE TABLE " + LinkGenresMovies.TABLE_NAME + " (" +
 
             LinkGenresMovies._ID        + " INTEGER PRIMARY KEY AUTOINCREMENT "         +
@@ -65,6 +79,7 @@ public class MovieDbHelper extends SQLiteOpenHelper {
             // Create tables in a single transaction
             sqLiteDatabase.execSQL(CREATE_MOVIES_TABLE);
             sqLiteDatabase.execSQL(CREATE_GENRES_TABLE);
+            sqLiteDatabase.execSQL(CREATE_TRAILERS_TABLE);
             sqLiteDatabase.execSQL(CREATE_LINK_GENRES_MOVIES_TABLE);
 
             sqLiteDatabase.setTransactionSuccessful();
